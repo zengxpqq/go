1380b51370dea9c04eba44e70716f67f8a30965d
曾祥鹏 <785251027@qq.com>
2022-03-11 10:19:43 +0800

    // 给你一个数组，将数组中的元素向右轮转 k 个位置，其中 k 是非负数。

--- codes/leetcode/sort/sort_test.go
+++ codes/leetcode/sort/sort_test.go
@@ -10,3 +10,33 @@ func TestSort(t *testing.T) {
 	result := sortedSquares(data)
 	fmt.Println(result)
 }
+
+func TestRotate(t *testing.T) {
+	rotate([]int{1, 2, 3, 4, 5, 6, 7}, 1)
+	rotate([]int{1, 2, 3, 4, 5, 6, 7}, 2)
+	rotate([]int{1, 2, 3, 4, 5, 6, 7}, 3)
+	rotate([]int{1, 2, 3, 4, 5, 6, 7}, 4)
+	rotate([]int{1, 2, 3, 4, 5, 6, 7}, 5)
+	rotate([]int{1, 2, 3, 4, 5, 6, 7}, 6)
+	rotate([]int{1, 2, 3, 4, 5, 6, 7}, 7)
+	rotate([]int{1, 2, 3, 4, 5, 6, 7}, 8)
+
+	rotate([]int{1, 2}, 1)
+	rotate([]int{1}, 1)
+}
+
+func TestRotate1(t *testing.T) {
+	data := []int{1, 2, 3, 4, 5, 6, 7}
+	rotate1(data, 8)
+	//rotate1(data, 1)
+	//rotate1(data, 2)
+	//rotate1(data, 3)
+	//rotate1(data, 4)
+	//rotate1(data, 5)
+	//rotate1(data, 6)
+	//rotate1(data, 7)
+	//rotate1(data, 8)
+	//rotate1([]int{1, 2}, 1)
+	//rotate1([]int{1}, 1)
+	fmt.Println(data)
+}
